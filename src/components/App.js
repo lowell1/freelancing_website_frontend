40ed74f6f669e8028dac20eb87e4c56bfa2398db
Lowell <29132034+lowell1@users.noreply.github.com>
2022-01-19 17:57:00 -0800

    eradicating material ui!

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TopNavigation from "./TopNavigation";
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavigation />
+    </div>
+  );
+}
+
+export default App;
