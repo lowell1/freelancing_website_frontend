71b1ba11c4c2e269ca291d20f462afee3c6df557
Lowell <29132034+lowell1@users.noreply.github.com>
2022-02-27 13:17:18 -0800

    fetch gigs from api

--- src/components/Gigs.js
+++ src/components/Gigs.js
@@ -1,19 +1,36 @@
-import React from "react";
-import { Switch, Route, useRouteMatch, useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+// import { Switch, Route, useRouteMatch, useParams } from "react-router-dom";
 
 export default function Gigs() {
-  return (
-    <>
-      gigs page{" "}
-      <Switch>
-        <Route path={useRouteMatch().path + "/:gigId"}>
-          <Gig />
-        </Route>
-      </Switch>
-    </>
-  );
+  const [gigData, setGigData] = useState(null);
+  const [fetchError, setFetchError] = useState(null);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_BASE_URL}/gigs`)
+      .then((res) => res.json())
+      .then(setGigData)
+      .catch(setFetchError);
+  }, []);
+
+  if (fetchError) {
+    console.log(fetchError);
+    return <>Could not load gigs :(</>;
+  } else if (gigData === null) {
+    return <>Loading gigs...</>;
+  } else {
+    return (
+      <>
+        {JSON.stringify(gigData)}
+        {/* <Switch>
+          <Route path={useRouteMatch().path + "/:gigId"}>
+            <Gig />
+          </Route>
+        </Switch> */}
+      </>
+    );
+  }
 }
 
-function Gig() {
-  return <>gig id: {useParams().gigId}</>;
-}
+// function Gig() {
+//   return <>gig id: {useParams().gigId}</>;
+// }
